3cdaa06a1323155fa8275b0f559446d518a8b2f9
Badalmishra <badalmishradev@gmail.com>
2019-01-17 16:00:17 +0530

    Status functionallity added

---
+++ README.md
@@ -0,0 +1,10 @@
+# todo-in-c
+## Singly Link list used to make a simple todo app in c
+### Add task
+### Remove task by id
+## remaining implementations
+### task status
+#### show active tasks functions
+#### show completed tasks
+#### update task status 
+### switch task order(lets see)
--- link.c
+++ link.c
@@ -1,22 +1,16 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 #include<string.h>
 
-struct Node
-{
+struct Node{
 struct Node *next;
-char * task;
+char task[100];
 int status;
 };
-
-void push(struct Node** head_ref, char* task)
-{
+void push(struct Node** head_ref, char task[100]){
     struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
-    char *temp=malloc(sizeof(*task));
-    strcpy(temp,task);
-    new_node->task=temp;
+    strcpy(new_node->task,task);
     new_node->status=1;
     /* 3. Make next of new node as head */
     new_node->next = (*head_ref);
@@ -24,10 +18,32 @@ void push(struct Node** head_ref, char* task)
     /* 4. move the head to point to the new node */
     (*head_ref) = new_node;
 }
-
-
-void remove_at(int index,struct Node** head_ref)
-{
+void update_status(int index,struct Node** head_ref){
+    struct Node * target_node;
+    target_node=*head_ref;
+    int i;
+    for( i=1;i<index;i++){
+        target_node=target_node->next;
+    }
+    printf("\n status of %s was %d\n",target_node->task,target_node->status);
+    target_node->status=(!target_node->status);
+    printf(" Its now changed to %d \n",target_node->status);
+    getch();
+}
+void show_active(struct Node** head_ref){
+    struct Node * traverser=*head_ref;
+    int i=1;
+    printf("\n List of active tasks\n");
+    while(traverser!=NULL){
+        if(traverser->status==1){
+            printf(" %d || %s \n",i,traverser->task);
+        }
+        i++;
+        traverser=traverser->next;
+    }
+    getch();
+}
+void remove_at(int index,struct Node** head_ref){
     struct Node* prev_node ,* target_node;
     prev_node = *head_ref;
     if(index==1){
@@ -42,9 +58,7 @@ void remove_at(int index,struct Node** head_ref)
     prev_node->next=target_node->next;
     free(target_node);
 }
-
-void printList(struct Node *node)
-{
+void printList(struct Node *node){
     int i =1;
     while (node != NULL){
 	printf("\n %d -> %s | status: %d", i , node->task,  node->status);
@@ -52,15 +66,15 @@ void printList(struct Node *node)
 	i++;
     }
 }
-
-int main()
-{
+int main(){
         int pos,choice;
         struct Node* head = NULL;
         char task[100] ="eat";
         while(1){
+            system("cls");
+            printList(head);
             printf("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-            printf("\nChoose || 1: to add task. || 2: to remove task || 11 to exit \n");
+            printf("\nChoose \n 1: to add task. || 2: to remove task \n 3: for active tasks 4: to update task status|| 11 to exit \n");
             printf("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
             scanf("%d",&choice);
             if(stdin)
@@ -76,14 +90,20 @@ int main()
                     scanf("%d",&pos);
                     remove_at(pos,&head);
                     break;
+                case 3:
+                    show_active(&head);
+                    break;
+                case 4:
+                    printf("please enter task number to be updated \n");
+                    scanf("%d",&pos);
+                    update_status(pos,&head);
+                    break;
                 case 11:
                     exit(0);
                     break;
                 default: printf("\n please follow instruction \n");
 
             }
-            system("cls");
-            printList(head);
         }
         return 0;
 }
